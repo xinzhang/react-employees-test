7f2a9198fbe5cec0cdfe883c0135763e95fdffb8
Xin Zhang <xinzhang@Xins-MacBook-Pro.local>
2019-12-05 20:47:00 +1100

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Grid from './components/grid/Grid';
 import './styles/Header.css';
 
 function App() {
   return (
+    <>
     <div className="header">
       <Header />      
-    </div>   
+    </div>
+    <Grid />
+    </>
   );
 }
 
--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import '../../styles/Grid.css'
 
-function Grid() {
+function Grid() {  
+  const createCards = (cardCount) => {
+    const cards = [];
+    // Outer loop to create parent
+    for (let i = 0; i < cardCount; i++) {
+      cards.push(<div className="card" />);
+    }
+    return cards;
+  }
+
   return (
     <div>
-      { 
-        for (let i=0; i<=15; i++) {
-          <Card />
-        }        
-      }
+      {createCards()}      
     </div>
   )
 }
